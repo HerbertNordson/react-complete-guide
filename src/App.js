9660735c762d449bed714c2814bebadd3e460c64
Herbert Nordson <herbert_nordson@hotmail.com>
2021-10-27 22:47:12 -0300

    feat: Create new components and include css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Expenseitem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+		  <h2>Let's get started!</h2>
+		  <Expenseitem></Expenseitem>
     </div>
   );
 }
